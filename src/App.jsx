33763df1f630a5d3a6023c16c3598e356520f0b7
Abril Contreras Suaste <contrerass.cristina@gmail.com>
2025-11-20 13:17:04 -0600

    Añadiendo font, editando colores y componentes

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,9 @@ import Header from './components/Header'
 
 export default function App(){
   return (
-    <div className="min-h-screen bg-gray-50 text-gray-900">
+    <div>
       <Header />
-      <main className="p-8">
-        <section className="max-w-4xl mx-auto bg-white rounded-lg shadow p-10">
-          <h1 className="text-3xl font-bold mb-4">Bienvenido a React + Tailwind</h1>
-          <p className="text-gray-600">Este es un proyecto inicializado con Vite y TailwindCSS. Desde aquí puedes crear tus componentes y páginas.</p>
-        </section>
-      </main>
+  
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,12 @@ export default function Header(){
   return (
     <header className="bg-white shadow">
       <div className="max-w-6xl mx-auto px-4 py-4 flex items-center justify-between">
-        <div className="text-xl font-semibold">Mi Proyecto</div>
+        <div className="text-xl font-AntonCustom">L.A Store</div>
         <nav>
           <ul className="flex gap-4 text-sm text-gray-600">
-            <li>Inicio</li>
-            <li>Docs</li>
+            <li>Juegos</li>
+            <li>Nosotros</li>
+            <li>FQA</li>
             <li>Contacto</li>
           </ul>
         </nav>
